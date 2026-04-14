ddb0f863a6e18e4a7dd5c547d7c8d1b9e0508240
Victor Gomez <vhgomez.py@gmail.com>
2026-04-14 08:41:53 -0300

    fix lint errors and add convex own tsconfig

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,9 +1,9 @@
 import { createClient, type GenericCtx } from "@convex-dev/better-auth";
 import { convex, crossDomain } from "@convex-dev/better-auth/plugins";
 import { components } from "./_generated/api";
-import { DataModel } from "./_generated/dataModel";
+import type { DataModel } from "./_generated/dataModel";
 import { query } from "./_generated/server";
-import { betterAuth, type BetterAuthOptions } from "better-auth/minimal";
+import { betterAuth } from "better-auth/minimal";
 import authConfig from "./auth.config";
 
 const siteUrl = process.env.SITE_URL!;
